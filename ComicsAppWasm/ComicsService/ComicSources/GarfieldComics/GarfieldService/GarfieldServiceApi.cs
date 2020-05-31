0442c8a9ae7b6a66f2ab05b171630f66a213a900
Parag <parag.raut@outlook.com>
2020-05-31 15:33:43 +0200

    Fixed first launch date

--- ComicsAppWasm/ComicsService/ComicSources/GarfieldComics/GarfieldService/GarfieldServiceApi.cs
+++ ComicsAppWasm/ComicsService/ComicSources/GarfieldComics/GarfieldService/GarfieldServiceApi.cs
@@ -16,7 +16,7 @@ namespace ComicsAppWasm.ComicsService.ComicSources.GarfieldComics.GarfieldServic
         private string GetRandomDateRange()
         {
             Random random = new Random();
-            DateTime start = new DateTime(1976, 6, 19);
+            DateTime start = new DateTime(1978, 6, 19);
             int range = (DateTime.Today - start).Days;
             var timeString = start.AddDays(random.Next(range)).ToString("yyyy-MM-dd");
             timeString = $"{timeString.Split("-")[0]}/{timeString}";
